da97a9377353b3da441280ddcba5600a4523c377
DivyaNairCent <dnair8@my.centennialcollege.ca>
2021-03-03 12:18:36 +0530

    DB url change

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,4 @@
 module.exports =
 {
-    //"URI": "mongodb://localhost/book_store"
     "URI": "mongodb+srv://divya:LyFBAeqlWAH7xBDT@mongodbserver.kjwox.mongodb.net/book_store?retryWrites=true&w=majority"
 }
